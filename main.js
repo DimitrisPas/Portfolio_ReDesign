60632799c66ba52f117e563aef30184f6fa6ab77
Dimitris Pasvantis <jim.pasvadis@yahoo.com>
2022-02-20 14:21:34 +0200

    Last portfolio desgin i swear

---
+++ main.js
@@ -0,0 +1,90 @@
+const cursor = document.querySelector(".test");
+
+const anchor = document.getElementsByTagName("a");
+
+const burger = document.querySelector(".burger-container");
+
+const close = document.querySelector(".close");
+
+const nav = document.querySelector(".nav__list");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+navLink.forEach((n) => {
+  n.addEventListener("click", () => {
+    nav.classList.remove("menu-opened");
+    document.documentElement.style.overflow = "visible";
+  });
+});
+
+burger.addEventListener("click", () => {
+  nav.classList.add("menu-opened");
+  document.documentElement.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("menu-opened");
+  document.documentElement.style.overflow = "visible";
+});
+
+// CUSTOM CURSOR
+
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 2) + "px; left: " + (e.pageX - 2) + "px;"
+  );
+});
+
+for (let i = 0; i < anchor.length; i++) {
+  anchor[i].addEventListener("mouseover", () => {
+    cursor.classList.remove("cursor");
+    cursor.classList.add("cursor2");
+  });
+}
+
+for (let i = 0; i < anchor.length; i++) {
+  anchor[i].addEventListener("mouseout", () => {
+    cursor.classList.remove("cursor2");
+    cursor.classList.add("cursor");
+  });
+}
+
+// CUSTOM TITLE WHEN TABBED OUT
+
+document.addEventListener("visibilitychange", () => {
+  if (document.visibilityState == "visible") {
+    document.title = "DP.";
+  } else {
+    document.title = "I miss you, Please come back 😔";
+  }
+});
+
+// CHANGE THINGS ON SCROLL
+
+window.onscroll = () => {
+  const header = document.getElementById("header");
+  if (this.scrollY >= 250) {
+    document.body.style.background = "#efe5df";
+    header.style.background = "#efe5df";
+  } else {
+    document.body.style.background = "#e9e6e3";
+    header.style.background = "#e9e6e3";
+  }
+};
+
+// GSAP ANIMATIONS
+let tl = gsap.timeline({defaults: {ease: "power4.inOut", duration: "1"}});
+const hero__title = document.querySelector(".hero__title");
+const project__info = document.querySelectorAll(".project__info");
+const project__image = document.querySelectorAll(".project__image");
+
+tl.to(".header", {top: "0", duration: 1.3}).to(
+  ".hero__title, .short-desc",
+  {
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: "1",
+    y: 0,
+  },
+  "+=.5"
+);
